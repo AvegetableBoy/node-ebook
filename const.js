39396856fa12e056e642c730c9f8f5c114086cd8
Sonny <1203987152@qq.com>
2021-04-16 16:12:59 +0800

    添加生产环境数据库配置

--- const.js
+++ const.js
@@ -6,49 +6,49 @@ let dbHost
 let dbUser
 let dbPwd
 if (env === 'dev') {
-  resUrl = 'http://172.25.29.205:8081'
-  mp3FilePath = '/Users/sam/Desktop/resource/mp3'
-  dbHost = 'localhost'
-  dbUser = 'root'
-  dbPwd = '123456789'
+    resUrl = 'http://172.25.29.205:8081'
+    mp3FilePath = '/Users/sam/Desktop/resource/mp3'
+    dbHost = 'localhost'
+    dbUser = 'root'
+    dbPwd = '123456789'
 } else if (env === 'prod') {
-  resUrl = 'http://106.15.231.180'
-  mp3FilePath = '/root/nginx/upload/mp3'
-  dbHost = '106.15.231.180'
-  dbUser = 'root'
-  dbPwd = 'Abcd123456.'
+    resUrl = '106.15.11.230'
+    mp3FilePath = '/root/nginx/upload/mp3'
+    dbHost = '106.15.11.230'
+    dbUser = 'root'
+    dbPwd = '123456789'
 }
 
 const category = [
-  'Biomedicine',
-  'BusinessandManagement',
-  'ComputerScience',
-  'EarthSciences',
-  'Economics',
-  'Engineering',
-  'Education',
-  'Environment',
-  'Geography',
-  'History',
-  'Laws',
-  'LifeSciences',
-  'Literature',
-  'SocialSciences',
-  'MaterialsScience',
-  'Mathematics',
-  'MedicineAndPublicHealth',
-  'Philosophy',
-  'Physics',
-  'PoliticalScienceAndInternationalRelations',
-  'Psychology',
-  'Statistics'
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
 ]
 
 module.exports = {
-  resUrl,
-  category,
-  mp3FilePath,
-  dbHost,
-  dbUser,
-  dbPwd
+    resUrl,
+    category,
+    mp3FilePath,
+    dbHost,
+    dbUser,
+    dbPwd
 }
